2bcc6b53e173906b6f0a7add50081f3565852034
TranThienDatLaGi <tranthiendatlagi@gmail.com>
2023-10-15 08:38:10 +0700

    upload

---
+++ src/main/java/vn/edu/iuh/fit/backend/resources/CandidateResources.java
@@ -0,0 +1,11 @@
+package vn.edu.iuh.fit.backend.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import vn.edu.iuh.fit.backend.services.CandidateServices;
+
+@RestController("/api/vi")
+public class CandidateResources {
+    @Autowired
+    private CandidateServices services;
+}
---
+++ src/test/java/vn/edu/iuh/fit/javawww_labweek05/JavaWwwLabweek05ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.javawww_labweek05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWwwLabweek05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
